6d30eb7faa745f3b63cf15256d7f6e9a2e065137
UserName <email.domain.com>
2018-03-04 23:32:24 -0300

    1st commit

---
+++ tcc_app_slim/src/dependencies.php
@@ -0,0 +1,48 @@
+<?php
+// DIC configuration
+
+$container = $app->getContainer();
+
+// view renderer
+$container['renderer'] = function ($c) {
+    $settings = $c->get('settings')['renderer'];
+    return new Slim\Views\PhpRenderer($settings['template_path']);
+};
+
+// monolog
+$container['logger'] = function ($c) {
+    $settings = $c->get('settings')['logger'];
+    $logger = new Monolog\Logger($settings['name']);
+    $logger->pushProcessor(new Monolog\Processor\UidProcessor());
+    $logger->pushHandler(new Monolog\Handler\StreamHandler($settings['path'], $settings['level']));
+    return $logger;
+};
+
+// $container['db'] = function($c) {
+//     $db = $c['settings']['db'];
+//     $host = "tccappdb.c9gqmcom8enw.us-east-2.rds.amazonaws.com";
+//     $dbname = "tcc_app_marcos";
+//     $user = "tcc_marcos";
+//     $pass = "!Aa456202";
+//     $pdo = new PDO('pgsql:host=' . $host . ';dbname=' . $dbname,$user, $pass);
+//     // PDO("pgsql:host=localhost;port=;dbname=bancoteste;user=root;password=12345
+//     // $pdo = new PDO("pgsql:host=" . $host . ";dbname=" . $dbname . "; user=" . $user . ";password=" . $pass);
+//     //  $user, $pass);
+//     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+//     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+//   return $pdo;
+// };
+$container['db'] = function($c) {
+    $db = $c['settings']['db'];
+    $host = "localhost";
+    $dbname = "tcc_app";
+    $user = "postgres";
+    $pass = "postgres";
+    $pdo = new PDO('pgsql:host=' . $host . ';dbname=' . $dbname,$user, $pass);
+    // PDO("pgsql:host=localhost;port=;dbname=bancoteste;user=root;password=12345
+    // $pdo = new PDO("pgsql:host=" . $host . ";dbname=" . $dbname . "; user=" . $user . ";password=" . $pass);
+    //  $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+  return $pdo;
+};
---
+++ tcc_app_slim/src/routes.php
@@ -0,0 +1,39 @@
+<?php
+
+use Slim\Http\Request;
+use Slim\Http\Response;
+
+
+// Routes
+//$app->get('/hello/{name}', function ($request, $response, $args) {
+////    return $response->write("Hello " . $args['name']);
+//    $string = "hello " . $args['name'];
+//    return $response->withJson($string);
+//});
+$app->get('/games', function (Request $request, Response $response, array $args) {
+
+    $stmt = $this->db->query("SELECT * FROM games");
+    $games = [];
+    while ($row = $stmt->fetch(\PDO::FETCH_ASSOC)) {
+        $games[] = [
+            'id' => $row['id'],
+            'name' => $row['name']
+        ];
+    }
+    return $response->withJson($games);
+
+});
+$app->get('/users', function (Request $request, Response $response, array $args) {
+
+    $str = ['nome'=>'Marcos Oleiro', 'idade' =>'31 anos'];
+    return $response->withJson($str);
+
+});
+
+$app->get('/[{name}]', function (Request $request, Response $response, array $args) {
+    // Sample log message
+    $this->logger->info("Slim-Skeleton '/' route");
+
+    // Render index view
+    return $this->renderer->render($response, 'index.phtml', $args);
+});

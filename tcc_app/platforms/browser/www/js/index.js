8e5cc7b8ea5a709c6d5989e4ef40a74d9afdb3ec
UserName <email.domain.com>
2018-03-07 04:20:54 -0300

    metodo post para salvar usuario pronto

--- tcc_app/platforms/browser/www/js/index.js
+++ tcc_app/platforms/browser/www/js/index.js
@@ -1,3 +1,20 @@
-document.addEventListener('deviceready', function(){
+// var f = document.getElementById("btn_reg");
 
-})
+// document.addEventListener('deviceready', function(){
+//     f.addEventListener('click',RegisterUser)
+
+// });
+
+// function RegisterUser(){
+//     var endpoint = " http://localhost:8080/users";
+
+//     var XHR = new XMLHttpRequest();
+//     var FD = new FormData();
+
+//     for (name in data){
+//         FD.append(name, data[name]);
+//     }
+
+//     XHR.open('POST', endpoint);
+//     XHR.send(FD);
+// }
--- tcc_app_slim/src/routes.php
+++ tcc_app_slim/src/routes.php
@@ -23,13 +23,54 @@ $app->get('/games', function (Request $request, Response $response, array $args)
     return $response->withJson($games);
 
 });
+
+// método para adicionar usuários ao banco de dados  (Registro de novos usuários)
 $app->post('/users', function (Request $request, Response $response, array $args) { 
-    // $aux = $this->request->getBody();
-    $aux = $this->request->getParsedBody();
-    $aux2 ;
 
-    return $this->response->write(print_r(array_keys($aux)));
+    // // vetor com as informações que vem via formulário
+    // $user_data = $this->request->getParsedBody();
 
+    // // string para camulflar a string no banco de dados
+    // $stringPass = "nirvana"; 
+    // // senha gerada (concatenada com uma string e gerada a hash)
+    // $user_data['passwd'] = hash('sha256',$user_data['passwd'] . $stringPass );
+
+    // // verificar se o e-mail já está cadastrado
+
+    // $aux = "example@domain.com";
+    // $stmt = $this->db->prepare("SELECT * FROM users WHERE email = :email");
+    // // $stmt->bindParam(':email',$user_data['email']);
+    // $stmt->bindParam(':email',$aux);
+    // $stmt->execute();
+    // $row = $stmt->fetch(); // se não for encontrado resultado, o fetch retorna false
+
+    // if ( $row != false ) {
+    //     return $this->response->write("E-mail já cadastrado.");
+    // }
+
+    // $stmt = null;
+    // $row = null;
+
+
+    // // verificar se o nickname já está cadastrado
+    // $stmt = $this->db->prepare("SELECT * FROM users WHERE nickname = :nickname");
+    // $stmt->bindParam(':nickname',$user_data['nickname']);
+    // $stmt->execute();
+    // $row = $stmt->fetch(); // se não for encontrado resultado, o fetch retorna false
+    
+    // if ( $row != false ) {
+    //     return $this->response->write("Nome de usuário já cadastrado.");
+    // }
+
+    // // salva no banco
+    // $stmt = $this->db->prepare ("INSERT INTO users (nickname, email,passwd) VALUES(:nickname,:email,:passwd)");
+    // $stmt->bindParam(':nickname', $user_data['nickname']);
+    // $stmt->bindParam(':email', $user_data['email']);
+    // $stmt->bindParam(':passwd', $user_data['passwd']);
+    // $stmt->execute();
+    
+    // return $this->response->write("Usuário cadastrado!.");
+    return $this->response->write("Funcionou!");
 });
 
 $app->get('/[{name}]', function (Request $request, Response $response, array $args) {

220b91cc613a73fbe99a30a1354dcf6cb11a00be
Marcos <marcos@devopers.com.br>
2018-03-09 18:56:34 -0300

    data from form

--- tcc_app/platforms/browser/www/js/register.js
+++ tcc_app/platforms/browser/www/js/register.js
@@ -1,21 +1,33 @@
 var btn_form = document.querySelector('#btn_reg')
 var divTestev = document.querySelector('div.teste');
+var fomr_register = document.querySelector('#register_form');
 
 
 
-document.addEventListener('deviceready', function() {
-    btn_form.addEventListener('click', SendFormData);
+document.addEventListener('deviceready', function () {
+
+    btn_form.addEventListener('click', SendFormData());
 });
 
-function SendFormData (){
-    
-    var xhr = new XMLHttpRequest();
-    var form = document.querySelector('#register_form');
-    // var fd = new FormData(form); 
-    var array_form = $('#register_form').serializeArray();
+function SendFormData() {
+    var form_value;
+    $("#register_form").submit(function (event) {
+        var form_value = ($(this).serializeArray());
 
-    console.log(array_form.toString());
-    divTestev.textContent = (array_form.toString());
-    
 
+
+        console.log("Nickname: " . concat(form_value[0].value));
+
+        console.log("E-mail: " . concat(form_value[1].value));
+
+        console.log("Senha : " . concat(form_value[2].value));
+
+        console.log("Confirmação de senha: " . concat(form_value[3].value));
+
+
+        event.preventDefault();
+    });
+
+    // event.preventDefault();
 }
+
